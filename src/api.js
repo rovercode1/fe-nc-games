85b843e9b4885836af359f9adc194fc1c248e5a9
Hayden <haydenworden@outlook.com>
2023-03-13 16:05:54 +0000

    feat - 🔵 Single reviews can be displayed + basic styling

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 const baseUrl = 'https://games-forum.onrender.com/api'
+
 export const fetchSingleReview = (review_id) => {
   return axios
     .get(baseUrl + '/reviews/' + review_id)
     .then((res) => {
-      console.log(res.review)
-      return res.review
+      return res.data.review
     })
     .catch((err) => {
       console.error(err);
--- src/components/SingleReview.jsx
+++ src/components/SingleReview.jsx
@@ -1,14 +1,45 @@
-import { useEffect, useEffect } from "react"
-const [singleReview, setSingleReview] = useState({})
-const SingleReview = ()=>{
+import { useEffect, useState } from "react";
+import { fetchSingleReview } from "../api";
+export default function SingleReview() {
+  const [singleReview, setSingleReview] = useState({});
+  const review_id = 2;
 
-const review_id = 2;
-useEffect(()=>{
-  fetchSingleReview(review_id).then((review)=>{
-    console.log(review)
-  })
-})
-return (
-  
-)
+  useEffect(() => {
+    fetchSingleReview(review_id).then((review) => {
+      setSingleReview(review);
+    });
+  }, []);
+
+  const displaySingleReview = (review) => {
+    return (
+      <div className="single-review-card" key={review.review_id}>
+        <div className="review-card-header">
+          <p>Posted by {review.owner}</p>
+          <p>{review.created_at}</p>
+        </div>
+        <div className="review-card-body">
+          <h2>{review.title}</h2>
+          <img src={review.review_img_url} alt={review.title}></img>
+          <p>{review.review_body}</p>
+        </div>
+        <div className="review-card-footer">
+          <div className="review-card-tags">
+            <span>
+              <p className="single-category"> {review.category}</p>{" "}
+            </span>
+            <span>
+              <p className="single-designer">{review.designer}</p>
+            </span>
+          </div>
+          <div className="review-card-stats">
+            <p>{review.votes} Votes</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <section id="single-review">{displaySingleReview(singleReview)}</section>
+  );
 }
