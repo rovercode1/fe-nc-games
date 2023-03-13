24080e82f5c5db143bbd2a6a307d7c0ed4d6e745
Hayden <haydenworden@outlook.com>
2023-03-13 17:00:03 +0000

    fix - 🟢 Axios fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import SingleReview from './components/SingleReview'
+import { useState } from 'react';
 function App() {
+  const [review_id, setReviewId] = useState(1)
+  const [isLoading, setIsLoading] = useState(true)
+
   return (
     <div className="App">
       <Routes>
-        <Route path='/reviews/:review_id' element={<SingleReview/>}>
+        <Route path='/reviews/:review_id' element={<SingleReview review_id={review_id} isLoading={isLoading} setIsLoading={setIsLoading} />}>
         </Route>
       </Routes>
     </div>
--- src/components/SingleReview.jsx
+++ src/components/SingleReview.jsx
@@ -1,26 +1,31 @@
 import { useEffect, useState } from "react";
 import { fetchSingleReview } from "../api";
-export default function SingleReview() {
+import ReactTimeAgo from "react-time-ago";
+
+export default function SingleReview({ review_id, isLoading, setIsLoading }) {
   const [singleReview, setSingleReview] = useState({});
-  const [review_id, setReviewId] = useState(1)
 
   useEffect(() => {
+    setIsLoading(true);
     fetchSingleReview(review_id).then((review) => {
       setSingleReview(review);
+      setIsLoading(false);
     });
-  }, []);
+  }, [setIsLoading, review_id]);
 
   const displaySingleReview = (review) => {
+    console.log();
     return (
       <div className="single-review-card" key={review.review_id}>
         <div className="review-card-header">
           <p>Posted by {review.owner}</p>
-          <p>{review.created_at}</p>
+          
         </div>
         <div className="review-card-body">
           <h2>{review.title}</h2>
           <img src={review.review_img_url} alt={review.title}></img>
           <p>{review.review_body}</p>
+          
         </div>
         <div className="review-card-footer">
           <div className="review-card-tags">
@@ -32,14 +37,18 @@ export default function SingleReview() {
             </span>
           </div>
           <div className="review-card-stats">
-            <p>{review.votes} Votes</p>
+            <span><p>{review.votes} Votes</p></span>
+            <span>  {!isLoading ? (<p> Posted <ReactTimeAgo date={new Date(review.created_at)} locale="en-US" /> </p>) : (false)}</span>
+            
+          
           </div>
         </div>
       </div>
     );
   };
-
-  return (
+  return isLoading ? (
+    <h1>Loading...</h1>
+  ) : (
     <section id="single-review">{displaySingleReview(singleReview)}</section>
   );
 }
