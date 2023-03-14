f1d5156f3569ac4c82b6caed4f8221ae1d469fb6
Hayden <haydenworden@outlook.com>
2023-03-14 11:31:53 +0000

    FIX

--- src/api.js
+++ src/api.js
@@ -5,6 +5,16 @@ const forumsData = axios.create({
   timeout: 1000,
   headers: {'X-Custom-Header': 'foobar'}
 });
+export const fetchSingleReview = (review_id) => {
+  return forumsData.get('/reviews/' + review_id).then(({data})=>{
+    return data.review
+  })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+
 export const fetchAllReviews = () => {
   return forumsData.get('/reviews').then(({data})=>{
     return data.reviews
--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { BrowserRouter } from "react-router-dom";
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en.json'
+TimeAgo.addDefaultLocale(en)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
 );
