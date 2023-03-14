9cc0824dfcb8605cafbedf04178aadc98e60d3b7
Hayden <haydenworden@outlook.com>
2023-03-14 10:46:44 +0000

    .

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ArticleList from './components/ArticleList'
+import { useState } from 'react';
+function App() {
+const [isLoading, setIsLoading] = useState(true)
+  
+  return (
+    <div className="App">
+      <h1>Merge tet</h1>
+      <ArticleList isLoading={isLoading} setIsLoading={setIsLoading}/>
+    </div>
+  );
+}
+export default App;
