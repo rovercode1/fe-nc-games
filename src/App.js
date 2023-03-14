e4aff0f488c867d604953027fb02f49f868ea202
Hayden <haydenworden@outlook.com>
2023-03-14 10:13:32 +0000

    feat - 🔵 Comments for single reviews route + basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import SingleReviewComments from './Components/SingleReviewComments';
+import { useState } from 'react';
 
 function App() {
+  const [isLoading, setisLoading] = useState(true)
   return (
     <div className="App">
-
+      <Routes>
+        <Route
+          path="/reviews/:review_id/comments"
+          element={<SingleReviewComments isLoading={isLoading} setIsLoading={setisLoading}/>}
+        />
+      </Routes>
     </div>
   );
 }
