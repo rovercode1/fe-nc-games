27bdfc011677831749fa6173fad1ea2cb0b433db
Hayden <haydenworden@outlook.com>
2023-03-18 11:13:29 +0000

    fix - 🟢 Remove unnecessary sortBy options + Default filter options

--- src/Components/FilterBar.jsx
+++ src/Components/FilterBar.jsx
@@ -7,25 +7,29 @@ export default function FilterBar({setSearchParams, setReviews}) {
   const [categories, setCategory] = useState([]);
   const sortBy =[
     "date",
-    "title",
-    "designer",
-    "owner",
-    "review_body",
-    "category",
     "votes",
     "comment_count"
   ]
   const [requestedSort, setRequestedSort] = useState({
     category: "",
-    sort_by: "",
-    order: "",
+    sort_by: "date",
+    order: "asc",
   });
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     fetchCategories().then((categories) => {
       setCategory(categories);
     });
-  }, []);
+  }, [requestedSort]);
+
+  
+
+  useEffect(()=>{
+    fetchReviewBySort(query).then((reviews)=>{
+      setReviews(reviews)
+    })
+  },[requestedSort, query, setReviews])
   
   const handleSubmit = () => {
     if(requestedSort.category === ''){
@@ -42,9 +46,7 @@ export default function FilterBar({setSearchParams, setReviews}) {
     }
     setSearchParams(requestedSort);
     const query = window.location.href.split('/')[3]
-    fetchReviewBySort(query).then((reviews)=>{
-      setReviews(reviews)
-    })
+    setQuery(query)
   };
 
   const handleChange = (e) => {
@@ -52,46 +54,48 @@ export default function FilterBar({setSearchParams, setReviews}) {
     handleSubmit()
   };
 
-  return (
-    <section id="sort-by">
-      <form
-        id="listing-form"
-        onSubmit={handleSubmit}>
-        <div className="input-box">
-          <label>Category</label>
-          <select type="select" id="category" onChange={handleChange}>
-            <option key="" value={""}></option>
-            {categories.map((category) => {
-              return (
-                <option key={category.slug} value={category.slug}>
-                  {capitalizeFirstLetter(category.slug.replaceAll('-',' '))}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-        <div className="input-box">
-          <label>Sort by</label>
-          <select type="select" id="sort_by" onChange={handleChange}>
-          <option key="" value={""}></option>
-            {sortBy.map((sort) => {
-              return (
-                <option key={sort} value={sort.toLowerCase()}>
-                  {capitalizeFirstLetter(sort.replaceAll("_", " "))}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-        <div className="input-box">
-          <label>Order</label>
-          <select type="select" id="order" onChange={handleChange}>
-          <option key="" value={""}></option>
-            <option value="ASC">asc</option>
-            <option value="DESC">desc</option>
-          </select>
-        </div>
-      </form>
-    </section>
-  );
+  const displayFilterBar = ()=>{
+    return (
+      <section id="sort-by">
+        <form
+          id="listing-form"
+          onSubmit={handleSubmit}>
+          <div className="input-box">
+            <label>Category</label>
+            <select type="select" id="category" onChange={handleChange}>
+              <option key="" value={""}>All reviews</option>
+              {categories.map((category) => {
+                return (
+                  <option key={category.slug} value={category.slug}>
+                    {capitalizeFirstLetter(category.slug.replaceAll('-',' '))}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="input-box">
+            <label>Sort by</label>
+            <select type="select" id="sort_by" onChange={handleChange}>
+              {sortBy.map((sort) => {
+                return (
+                  <option key={sort} value={sort.toLowerCase()}>
+                    {capitalizeFirstLetter(sort.replaceAll("_", " "))}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="input-box">
+            <label>Order</label>
+            <select type="select" id="order" onChange={handleChange}>
+              <option value="ASC">asc</option>
+              <option value="DESC">desc</option>
+            </select>
+          </div>
+        </form>
+      </section>
+    );
+  }
+
+  return displayFilterBar()
 }
