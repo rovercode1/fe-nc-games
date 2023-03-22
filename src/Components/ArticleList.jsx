5ce89719a15524c8ccdd95a58f9f4ad09b41bfc6
Hayden <haydenworden@outlook.com>
2023-03-22 08:28:55 +0000

    edit - 🟡 Small changes

--- src/Components/ArticleList.jsx
+++ src/Components/ArticleList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSearchParams, Link } from "react-router-dom";
+import ReactTimeAgo from "react-time-ago";
 
 import CategoryMenu from "./CategoryMenu";
 import FilterBar from "./FilterBar";
@@ -15,6 +16,7 @@ export default function ArticleList({ reviews, setReviews }) {
 
   const loadingReviewData = [
     {
+      id:1,
       owner: "lorem ip",
       created_at:'Lorem ipsum dolor sit.',
       title: "Lorem ipsum dolor sit amet consectetur a",
@@ -22,6 +24,7 @@ export default function ArticleList({ reviews, setReviews }) {
       comment_count: 2,
     },
     {
+      id:2,
       owner: "Lorem, ipsum.",
       title: " Lorem, ipsum dolor.",
       created_at:'Lorem ipsum dolor .',
@@ -29,6 +32,7 @@ export default function ArticleList({ reviews, setReviews }) {
       comment_count: 300,
     },
     {
+      id:3,
       owner: "Lorem, ipsum sit amet .",
       title: " Losit amet or.",
       created_at:'Lorem ipsum dolor sit.',
@@ -36,6 +40,7 @@ export default function ArticleList({ reviews, setReviews }) {
       comment_count: 0,
     },
     {
+      id:4,
       owner: "Lorem, ipsum sit a.",
       title: " Loresit amet m dolor.",
       created_at:'Lorem ipsum  sit.',
@@ -43,6 +48,7 @@ export default function ArticleList({ reviews, setReviews }) {
       comment_count: 30,
     },
     {
+      id:5,
       owner: "Lorem, ipsit amet .",
       title: " Lorem, ipssit amet .",
       created_at:'Lorem ipsum dolor sit.',
@@ -58,41 +64,51 @@ export default function ArticleList({ reviews, setReviews }) {
       setIsLoadingReviews(false);
     });
   }, [setReviews]);
+
+
+  const displayReviewCard = (review)=>{
+    return (    <article
+    id={review.review_id}
+    className="review-card"
+    key={review.review_id}
+  >
+    <div className="review-card-header">
+      <p>
+        Posted by <span>{review.owner}</span>
+      </p>
+        <p>Posted <ReactTimeAgo
+            date={new Date(review.created_at)}
+            locale="en-US"
+          />
+        </p>
+    </div>
+    <div className="review-body">
+      <Link to={`/reviews/${review.review_id}`}>
+        <img src={review.review_img_url} alt={review.title}></img>
+      </Link>
+      <div className="review-card-bottom">
+        <h3>{review.title}</h3>
+        <div className="review-card-button">
+          <button className="button" id={review.votes}>
+            {review.votes} Votes
+          </button>
+          <Link to={`/reviews/${review.review_id}`}>
+            <button className="button">
+              {review.comment_count} Comments
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  </article>)
+  }
+
   const displayReviews = () => {
     return (
       <section id="reviews-container">
         {reviews.map((review) => {
           return (
-            <article
-              id={review.review_id}
-              className="review-card"
-              key={review.review_id}
-            >
-              <div className="review-card-header">
-                <p>
-                  Posted by <span>{review.owner}</span>
-                </p>
-                <p>{review.created_at}</p>
-              </div>
-              <div className="review-body">
-                <Link to={`/reviews/${review.review_id}`}>
-                  <img src={review.review_img_url} alt={review.title}></img>
-                </Link>
-                <div className="review-card-bottom">
-                  <h3>{review.title}</h3>
-                  <div className="review-card-button">
-                    <button className="button" id={review.votes}>
-                      {review.votes} Votes
-                    </button>
-                    <Link to={`/reviews/${review.review_id}`}>
-                      <button className="button">
-                        {review.comment_count} Comment
-                      </button>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </article>
+            displayReviewCard(review)
           );
         })}
       </section>
@@ -104,7 +120,7 @@ export default function ArticleList({ reviews, setReviews }) {
     <section id="reviews-container">
      { loadingReviewData.map((review)=>{
       return(
-        <article className="review-card loading-review">
+        <article  key={review.id} className="review-card loading-review">
         <div className="review-card-header">
           <span>
             <p>{review.owner}</p>
--- src/Components/SingleReview.jsx
+++ src/Components/SingleReview.jsx
@@ -1,80 +1,102 @@
 import { useEffect, useState } from "react";
 import { fetchSingleReview } from "../api";
 import { useParams, Link } from "react-router-dom";
-import '../styles/SingleReview.css'
-export default function SingleReview({err, setError, comments }) {
+import ReactTimeAgo from "react-time-ago";
+import "../styles/SingleReview.css";
+export default function SingleReview({ err, setError, comments }) {
   const { review_id } = useParams();
   const [singleReview, setSingleReview] = useState([]);
-  const [isLoadingSingleReviews, setIsLoadingReviews] = useState(true)
+  const [isLoadingSingleReviews, setIsLoadingReviews] = useState(true);
 
   useEffect(() => {
     setIsLoadingReviews(true);
-    fetchSingleReview(review_id).then((review) => {
-      setSingleReview(review);
-      setIsLoadingReviews(false);
-    }).catch(({response})=>{
-      setIsLoadingReviews(false);
-      setError(response.data.msg)
-    })
+    fetchSingleReview(review_id)
+      .then((review) => {
+        setSingleReview(review);
+        setIsLoadingReviews(false);
+      })
+      .catch(({ response }) => {
+        setIsLoadingReviews(false);
+        setError(response.data.msg);
+      });
   }, [review_id, setIsLoadingReviews, setError]);
-
 
   const loadingData = [
     {
       owner: "lorem ip",
-      created_at:'Lorem ipsum dolor sit.',
+      created_at: "Lorem ipsum dolor sit.",
       title: "Lorem ipsum dolor sit amet consectetur a",
       votes: 100,
       comment_count: 2,
-    }
-  ]
+    },
+  ];
 
-  const loadingSingleReview = ()=>{
-  return (<article
-    className="single-review-card loading-single-card">
-    <div className="review-card-header">
-        <span> <p>orem ipm </p> </span>
-      <span><p>orem ipm dolooo lol</p></span>
-    </div>
-    <div className="review-card-body">
-      <span>
-      <h2>Lorem ipsum dolor sit  amet consamet consectetur a</h2>
-      </span>
-      <div id='single-loading-img'></div>
-      <span>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque officiis sit aspernatur nam quia ad eligendi sed, rerum neque voluptatibus cumque harum earum, modi, voluptatem commodi sunt enim illum labore possimus maiores sapiente iste. Rem quia ipsa voluptate ea id, cupiditate quo, optio alias nemo deleniti, accusantium corporis error veritatis iure eligendi tenetur porro ad mollitia beatae cumque a commodi? Quidem dolores nihil exercitationem molestias, neque fugit atque, dolore, ab aliquam dicta optio? Necessitatibus totam alias modi nesciunt quam placeat eos, vero voluptatem! Maiores corrupti aut consectetur facilis totam. Quam modi inventore tenetur velit cupiditate, alias consequatur eum officiis. Tempora!</p>
-      </span>
-      <div className="review-card-tags">
-        <span className="single-category">
-           lorem ips
-        </span>
-        <span className="single-designer">
-           consec
-        </span>
-      </div>
-    </div>
-    <div className="review-card-footer">
-      <div className="review-card-stats">
-        <button> x Votes</button>
-      </div>
-      <div className="review-card-stats">
-        <button> x Comments</button>
-      </div>
-    </div>
-  </article>)
-  }
+  const loadingSingleReview = () => {
+    return (
+      <article className="single-review-card loading-single-card">
+        <div className="review-card-header">
+          <span>
+            {" "}
+            <p>orem ipm </p>{" "}
+          </span>
+          <span>
+            <p>orem ipm dolooo lol</p>
+          </span>
+        </div>
+        <div className="review-card-body">
+          <span>
+            <h2>Lorem ipsum dolor sit amet consamet consectetur a</h2>
+          </span>
+          <div id="single-loading-img"></div>
+          <span>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque
+              officiis sit aspernatur nam quia ad eligendi sed, rerum neque
+              voluptatibus cumque harum earum, modi, voluptatem commodi sunt
+              enim illum labore possimus maiores sapiente iste. Rem quia ipsa
+              voluptate ea id, cupiditate quo, optio alias nemo deleniti,
+              accusantium corporis error veritatis iure eligendi tenetur porro
+              ad mollitia beatae cumque a commodi? Quidem dolores nihil
+              exercitationem molestias, neque fugit atque, dolore, ab aliquam
+              dicta optio? Necessitatibus totam alias modi nesciunt quam placeat
+              eos, vero voluptatem! Maiores corrupti aut consectetur facilis
+              totam. Quam modi inventore tenetur velit cupiditate, alias
+              consequatur eum officiis. Tempora!
+            </p>
+          </span>
+          <div className="review-card-tags">
+            <span className="single-category">lorem ips</span>
+            <span className="single-designer">consec</span>
+          </div>
+        </div>
+        <div className="review-card-footer">
+          <div className="review-card-stats">
+            <button> x Votes</button>
+          </div>
+          <div className="review-card-stats">
+            <button> x Comments</button>
+          </div>
+        </div>
+      </article>
+    );
+  };
 
   const displaySingleReview = (review, comments) => {
-    return isLoadingSingleReviews ?loadingSingleReview():(
+    return isLoadingSingleReviews ? (
+      loadingSingleReview()
+    ) : (
       <article
         id={review.review_id}
         className="single-review-card"
-        key={review.review_id}>
+        key={review.review_id}
+      >
         <div className="review-card-header">
           <p>
             Posted by <span>{review.owner}</span>
           </p>
-          <p>{review.created_at}</p>
+          <p>
+            Posted <ReactTimeAgo date={new Date(review.created_at)} locale="en-US" />
+          </p>
         </div>
         <div className="review-card-body">
           <h2>{review.title}</h2>
@@ -91,7 +113,7 @@ export default function SingleReview({err, setError, comments }) {
         </div>
         <div className="review-card-footer">
           <div className="review-card-stats">
-            <button> {review.votes} Votes</button>   
+            <button> {review.votes} Votes</button>
           </div>
           <div className="review-card-stats">
             <button> {comments.length} Comments</button>
@@ -101,24 +123,27 @@ export default function SingleReview({err, setError, comments }) {
     );
   };
 
-    const nonExistentReview = ()=>{
+  const nonExistentReview = () => {
     return (
-      <> 
-      <h1> Oops! this page doesn’t exist. </h1> 
-      <p> Vaild reviews will be displayed on the <Link to={'/'}>home</Link> page. </p>
+      <>
+        <h1> Oops! this page doesn’t exist. </h1>
+        <p>
+          {" "}
+          Vaild reviews will be displayed on the <Link to={"/"}>home</Link>{" "}
+          page.{" "}
+        </p>
       </>
     );
-  }
+  };
 
-  return  err ?  nonExistentReview():(
+  return err ? (
+    nonExistentReview()
+  ) : (
     <>
-    <section id="single-review">{displaySingleReview(singleReview, comments)}</section>
-    {/* <section id="single-review">{loadingSingleReview()}</section> */}
+      <section id="single-review">
+        {displaySingleReview(singleReview, comments)}
+      </section>
+      {/* <section id="single-review">{loadingSingleReview()}</section> */}
     </>
   );
-
-
-
-
-
 }
